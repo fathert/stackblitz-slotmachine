a1cccabc69637b489e27122360db13fd6ad9c072
fathert <tim@fathers.me.uk>
2024-06-23 13:19:58 +0100

    .

--- script.js
+++ script.js
@@ -1,96 +1,98 @@
-console.log('Hello!');
 
-merge();
+
+const symbols = [
+  './symbols/banana.svg',
+  './symbols/cherry.svg',
+  './symbols/lemon.svg',
+  './symbols/aubergine.svg',
+  './symbols/bell.svg',
+  './symbols/avocado.svg'
+];
+
 //for (var i = 0; i < 1000; i++) {
 const spins = 30;
-const spinTime = 1;
-const symbols = 6;
+const spinTime = 5;
+const symbolCount = 6;
 const symbolPad = 4;
 const symbolHeight = 100;
 
-const startY = 0;
-var endY = (symbolHeight + symbolPad) * symbols * spins;
+const reelHeight = 450;
+const stripLength = 624;
+const stripMargin = 0;
 
-const selectedSymbol = 0; //Math.floor((Math.floor(Math.random() * 6)));
+const startSymbol = 0; //Math.floor((Math.floor(Math.random() * 6)));
 
-endY += (symbols - selectedSymbol + 2) * (symbolHeight + symbolPad);
+var startY = stripLength - (startSymbol * (symbolHeight + symbolPad)) + (symbolPad / 2);
+startY += -((symbolHeight + symbolPad) / 2);
+startY += (reelHeight / 2);
+var endY = (symbolHeight + symbolPad) * symbolCount * spins;
+endY += (symbolCount - startSymbol + 2) * (symbolHeight + symbolPad);
+
+endY = (279 + spins * stripLength);
+
+console.log(`Start: ${startY}, end: ${endY}`);
 
 const animateTime = spinTime * 1000;
 
-// console.log(`Symbol ${i} Y: ${startY} -> ${endY}`);
-var strip = document.getElementById('strip');
 
-const animation = strip.animate(
-  [
-    { backgroundPositionY: `${startY}px`, easing: 'ease' },
-    { backgroundPositionY: `${endY}px`, easing: 'ease' },
-  ],
-  {
-    duration: animateTime,
-    iterations: 1,
-  }
-);
+merge(symbols).then((imageStrip) => {
+  var strip = document.getElementById('strip');
+  strip.style.backgroundImage = `url(${imageStrip.image})`;
 
-animation.onfinish = () => {
-  console.log('Finshed, setting to ', endY);
-  strip.style.backgroundPositionY = `${endY}px`;
-};
+  console.info(`${imageStrip} Strip ${imageStrip.width} x ${imageStrip.height}`);
 
-// animation.currentTime = i * offset;
-//}
+  const animation = strip.animate(
+    [
+      { backgroundPositionY: `${startY}px`, easing: 'ease' },
+      { backgroundPositionY: `${endY}px`, easing: 'ease' },
+    ],
+    {
+      duration: animateTime,
+      iterations: 1,
+    }
+  );
+  //animation.pause();
 
-// src="https://www.svgrepo.com/show/227289/banana.svg"
-// src="https://www.svgrepo.com/show/298621/cherry.svg"
+  animation.onfinish = () => strip.style.backgroundPositionY = `${endY}px`;
 
-function merge() {
+});
+
+function merge(symbols) {
   return new Promise((resolve, reject) => {
-    const img = new Image();
-    img.src = path;
-    img.onload = () => {
-      resolve(img);
-    };
-    img.onerror = (e) => {
-      reject(e);
-    };
-  });
-  
+    Promise.all(symbols.map((symbol) => loadImage(symbol)))
+      .then((images) => {
+        var canvas = document.createElement('canvas');
+        canvas.style.display = 'none';
+        const ctx = canvas.getContext('2d');
 
+        canvas.width = 100 + stripMargin * 2;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.globalAlpha = 1.0;
+        canvas.height = (symbolHeight + symbolPad) * images.length;
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  var canvas = document.getElementById('canvas'),
-    ctx = canvas.getContext('2d');
-
-  canvas.width = 100;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.globalAlpha = 1.0;
-
-  Promise.all([
-    loadImage('./symbols/banana.svg'),
-    loadImage('./symbols/cherry.svg'),
-    loadImage('./symbols/lemon.svg'),
-    loadImage('./symbols/aubergine.svg'),
-    loadImage('./symbols/bell.svg'),
-    loadImage('./symbols/avocado.svg'),
-  ]).then((images) => {
-    console.log('Images: ', images);
-    canvas.height = (symbolHeight + symbolPad) * images.length;
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    images.reduce((_, image, i) => {
-      console.log(i, image);
-      ctx.drawImage(
-        image,
-        0,
-        i * (symbolHeight + symbolPad),
-        100,
-        symbolHeight
-      );
-      return;
-    }, '');
-    var img = canvas.toDataURL('image/jpeg');
-    var strip = document.getElementById('strip');
-    strip.style.backgroundImage = `url(${img})`;
-  });
-}
+        images.reduce((start, image, i) => {
+          console.log(i, start);
+          ctx.drawImage(
+            image,
+            stripMargin,
+            start,
+            canvas.width,
+            symbolHeight
+          );
+          return start += (symbolHeight + symbolPad);
+        }, 0);
+        var image = canvas.toDataURL('image/jpeg');
+        canvas.remove();
+        resolve({
+          height: canvas.height,
+          width: canvas.width,
+          image
+        });
+      });
+  })
+};
 
 function loadImage(path) {
   console.log(`Loading: ${path}`);
